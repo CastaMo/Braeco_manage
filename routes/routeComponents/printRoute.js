f7a2c2e7632d60dbbefef4818cb4d87f7ba50df0
庄梓嘉 <646706396@qq.com>
2016-06-23 19:59:32 +0800

    print update

--- routes/routeComponents/printRoute.js
+++ routes/routeComponents/printRoute.js
@@ -3,8 +3,7 @@
 var getCallbackHandleForRequest = require("../../common/getCallbackHandleForRequest.js");
 
 module.exports = function(router) {
-
-  router.get("/Manage/Settings/Business/Print/Data", getCallbackHandleForRequest("GET"));
-
-  return router;
+	router.get('/Manage/Settings/Business/Print/Data', getCallbackHandleForRequest("GET"));
+	router.post('/Dinner/Printer/Update/:printerId', getCallbackHandleForRequest("POST"));
+	return router;
 };
